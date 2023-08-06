9929b16899cf9d89961ab41b8924b8b81f4c3367
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-08-06 16:16:46 +0900

    feat:wallet 연결

--- App.tsx
+++ App.tsx
@@ -1,12 +1,84 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import "react-native-get-random-values";
+import "react-native-url-polyfill/auto";
+import { Button, TouchableOpacity, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import * as Linking from "expo-linking";
+import bs58 from "bs58";
+import nacl from "tweetnacl";
+
+import React, {
+  Component,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import {
+  clusterApiUrl,
+  Connection,
+  PublicKey,
+  Keypair,
+  sendAndConfirmTransaction,
+  TransactionInstruction,
+  SystemProgram,
+  Transaction,
+} from "@solana/web3.js";
+//devnet연결
+const NETWORK = clusterApiUrl("devnet");
+//지갑링크
+const buildUrl = (path: string, params: URLSearchParams) =>
+  `https://phantom.app/ul/v1/${path}?${params.toString()}`;
 
 export default function App() {
+  const [dappKeyPair] = useState(nacl.box.keyPair());
+
+  //솔라나 키
+  const [deepLink, setDeepLink] = useState<string>("");
+  //솔라나 키
+  const [phantomWalletPublicKey, setPhantomWalletPublicKey] =
+    useState<PublicKey>();
+  /*Link */
+  const onConnectRedirectLink = Linking.createURL("onConnect");
+  //conntection
+  const connection = new Connection(NETWORK);
+  const handleDeepLink = ({ url }: Linking.EventType) => {
+    setDeepLink(url);
+  };
+  //wallet connection
+
+  const connect = async () => {
+    const params = new URLSearchParams({
+      dapp_encryption_public_key: bs58.encode(dappKeyPair.publicKey),
+      cluster: "devnet",
+      app_url: "https://phantom.app",
+      redirect_link: onConnectRedirectLink,
+    });
+
+    const url = buildUrl("connect", params);
+    Linking.openURL(url);
+  };
+  useEffect(() => {
+    (async () => {
+      const initialUrl = await Linking.getInitialURL();
+      if (initialUrl) {
+        setDeepLink(initialUrl);
+      }
+    })();
+    Linking.addEventListener("url", handleDeepLink);
+    // return () => {
+    //   Linking.removeEventListener("url", handleDeepLink);
+    // };
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={connect}>
+          <Text>연결</Text>
+        </TouchableOpacity>
+      </View>
+    </NavigationContainer>
   );
 }
 
